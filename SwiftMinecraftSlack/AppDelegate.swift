3809656499baee345059da4fb7381bceb2eb792f
Rhett Rogers <serenade.xs@icloud.com>
2015-10-09 13:21:38 -0600

    Initial Commit

---
+++ SwiftMinecraftSlack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftMinecraftSlack
+//
+//  Created by Rhett Rogers on 10/9/15.
+//  Copyright © 2015 Rhett Rogers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftMinecraftSlack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftMinecraftSlack
+//
+//  Created by Rhett Rogers on 10/9/15.
+//  Copyright © 2015 Rhett Rogers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
