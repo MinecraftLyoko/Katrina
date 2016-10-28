f4e8e890f5176657cee554f2b1a011263b8785db
Rhett Rogers <serenade.xs@icloud.com>
2016-10-28 15:08:33 -0600

    Updated to swift 3.  Will download and launch vanilla minecraft

--- Sources/MinecraftServer.swift
+++ Sources/MinecraftServer.swift
@@ -16,18 +16,18 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
     var playerCount: Int = 0
     var maxPlayers: Int = 0
     var serverActive = false
-    
+
     lazy var javaTask: Process = {
         let javaTask = Process()
         javaTask.currentDirectoryPath = MinecraftServer.bundlePath
         javaTask.launchPath = "/usr/bin/java"
         javaTask.arguments = ["-Xmx1024M", "-Xms1024M", "-jar", MinecraftServer.jarPath, "nogui"]
-        
+
         javaTask.standardOutput = self.out🚿
         javaTask.standardInput = self.in🚿
         return javaTask
     }()
-    
+
     static let bundlePath = Bundle.main.bundlePath + "/Contents/server"
     static let jarPath = "\(MinecraftServer.bundlePath)/minecraft_download.jar"
 
@@ -53,30 +53,30 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
         guard let versionManifestURL = URL(string: "https://launchermeta.mojang.com/mc/game/version_manifest.json") else { callback?(nil)
             return
         }
-        
+
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: versionManifestURL) { data, response, error in
             guard error == nil, let data = data, let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? [String: AnyObject] else {
                 callback?(nil)
                 return
             }
-            
+
             guard let latestObject = jsonData?["latest"] as? [String: String], let latestVersion = latestObject["release"] else {
                 callback?(nil)
                 return
             }
-            
+
             callback?(latestVersion)
         }
-        
+
         task.resume()
     }
-    
+
     func downloadServer(version: String = "latest") {
         if version == "latest" {
             latestVersion { version in
                 guard let version = version else { return }
-                
+
                 if !FileManager.default.fileExists(atPath: MinecraftServer.bundlePath) {
                     do {
                         try FileManager.default.createDirectory(atPath: MinecraftServer.bundlePath, withIntermediateDirectories: true, attributes: nil)
@@ -84,7 +84,7 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
                         print(error)
                     }
                 }
-                
+
                 if FileManager.default.fileExists(atPath: MinecraftServer.jarPath) {
                     do {
                         try FileManager.default.removeItem(atPath: MinecraftServer.jarPath)
@@ -92,24 +92,36 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
                         print(error)
                     }
                 }
-                
-                
+
+
                 if let URL = URL(string: "https://s3.amazonaws.com/Minecraft.Download/versions/\(version)/minecraft_server.\(version).jar") {
-                    
+                    print(URL)
                     let session = URLSession(configuration: .default)
-                    let task = session.downloadTask(with: URL)
+                    let task = session.downloadTask(with: URL) { downloadedPath, response, error in
+                        guard let downloadedPath = downloadedPath?.relativePath else {
+                            print("error downloading \(error)")
+                            return
+                        }
+                        do {
+                            try FileManager.default.moveItem(atPath: downloadedPath, toPath: MinecraftServer.jarPath)
+                            print("did finish")
+                            self.launch()
+                        } catch let error as Error {
+                            print("Couldn't move file \(error)")
+                        }
+                    }
                     task.resume()
                 }
-                
+
             }
         }
     }
 
     func launch() {
         killPreviousServersIfTheyExist()
-        
+
         let eulaPath = "\(MinecraftServer.bundlePath)/eula.txt"
-        
+
         if !FileManager.default.fileExists(atPath: eulaPath) {
             let string = "eula=true"
             let data = string.data(using: String.Encoding.utf8)
@@ -120,21 +132,21 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
         print("Starting java task with id \(javaTask.processIdentifier)")
         UserDefaults.standard.set(Int(javaTask.processIdentifier), forKey: "minecraft_task_id")
     }
-    
+
     func killPreviousServersIfTheyExist() {
         let taskID = UserDefaults.standard.integer(forKey: "minecraft_task_id")
-        
+
         let task = Process()
         task.launchPath = "/bin/bash"
         task.arguments = ["-c", "kill \(taskID)"]
         task.launch()
         task.waitUntilExit()
-        
+
     }
 
     func serverDidLoad() {
         serverActive = true
-        
+
         NotificationCenter.default.post(name: .serverRunning, object: nil)
     }
 
@@ -147,7 +159,7 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
             if log.contains("joined the game\n") {
                 let range = log.index(log.startIndex, offsetBy: 33) ..< log.endIndex
                 _ = log.substring(with: range).replacingOccurrences(of: " joined the game\n", with: "")
-                
+
             }
         }
     }
@@ -169,11 +181,11 @@ class MinecraftServer : NSObject, URLSessionDownloadDelegate {
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         guard let data = NSData(contentsOf: location as URL) else { return }
-        
+
         FileManager.default.createFile(atPath: MinecraftServer.jarPath, contents: data as Data, attributes: nil)
         launch()
     }
-    
+
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         print("\rDownload Progress: \(Int((Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)) * 100))%")
     }
